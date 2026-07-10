b6d0a3c48701e58facfaa5189576e54a845ae01a
oneachina <ruiruixi@hotmail.com>
2026-07-10 12:07:08 +0800

    feat: rewrite WeaponListener with ADS state machine and staged reload

--- src/main/kotlin/cn/oneachina/zombieRun/listener/WeaponListener.kt
+++ src/main/kotlin/cn/oneachina/zombieRun/listener/WeaponListener.kt
@@ -1,10 +1,16 @@
 package cn.oneachina.zombieRun.listener
 
 import cn.oneachina.zombieRun.ZombieRun
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.NamespacedKey
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.player.PlayerItemHeldEvent
+import org.bukkit.event.player.PlayerToggleSneakEvent
+import org.bukkit.persistence.PersistentDataType
 
 class WeaponListener(private val plugin: ZombieRun) : Listener {
 
@@ -13,18 +19,64 @@ class WeaponListener(private val plugin: ZombieRun) : Listener {
         val player = event.player
         val item = event.item ?: return
         val action = event.action
-
-        if (action != Action.RIGHT_CLICK_AIR && action != Action.RIGHT_CLICK_BLOCK) return
-
         val wm = plugin.weaponManager
 
-        if (wm.isZombieRunWeapon(item)) {
+        if (!wm.isZombieRunWeapon(item)) return
+
+        if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) {
             event.isCancelled = true
 
+            if (wm.isReloading(item)) return
+
             if (player.isSneaking) {
-                wm.reloadWeapon(player, item)
+                if (wm.getMagazine(item) >= (wm.getWeaponConfig(wm.getWeaponId(item) ?: return) ?: return).magazineSize) {
+                    player.sendMessage(Component.text("弹匣已满", NamedTextColor.GREEN))
+                    return
+                }
+                wm.handleReload(player, item)
             } else {
                 wm.handleShoot(player, item)
+            }
+        }
+    }
+
+    @EventHandler
+    fun onItemHeldChange(event: PlayerItemHeldEvent) {
+        val player = event.player
+        val prevItem = player.inventory.getItem(event.previousSlot)
+        if (prevItem != null && plugin.weaponManager.isZombieRunWeapon(prevItem)) {
+            if (plugin.weaponManager.isReloading(prevItem)) {
+                plugin.weaponManager.cancelReload(player, prevItem)
+            }
+        }
+        val newItem = player.inventory.getItem(event.newSlot)
+        if (newItem != null && plugin.weaponManager.isZombieRunWeapon(newItem)) {
+            val pdc = newItem.itemMeta?.persistentDataContainer
+            pdc?.set(NamespacedKey("zombie-run", "shot_count"), PersistentDataType.INTEGER, 0)
+            newItem.itemMeta = newItem.itemMeta
+        }
+    }
+
+    @EventHandler
+    fun onSneakToggle(event: PlayerToggleSneakEvent) {
+        val player = event.player
+        val item = player.inventory.itemInMainHand
+        val wm = plugin.weaponManager
+
+        if (!wm.isZombieRunWeapon(item)) {
+            if (wm.isAds(player)) {
+                wm.setAds(player, false)
+            }
+            return
+        }
+
+        if (event.isSneaking) {
+            if (wm.isReloading(item) && wm.getMagazine(item) < (wm.getWeaponConfig(wm.getWeaponId(item) ?: return) ?: return).magazineSize) {
+                wm.handleReload(player, item)
+            }
+        } else {
+            if (wm.isReloading(item)) {
+                wm.cancelReload(player, item)
             }
         }
     }
